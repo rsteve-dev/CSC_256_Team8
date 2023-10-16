424309baeb640c223e9a51bd44b5678092591fbf
ldbennett3 <doommarine93@gmail.com>
2023-10-16 19:35:30 -0400

    Added code comments

--- Selenium Lab/WebApp/main.py
+++ Selenium Lab/WebApp/main.py
@@ -7,11 +7,15 @@ app = Flask(__name__)
 @app.route("/")
 def start_app():
     csv_data = get_csv_data()
+    # Render the search.html file in templates folder and pass the csv data through
     return render_template('search.html', csv_data=csv_data)
 
+# Funtion to get the CSV file's data
 def get_csv_data():
     csv_data = []
+    # Open the file and force UTF-8 encoding
     with open('./static/books_short.csv', encoding="utf8") as file:
+        # Read the file and put each row in a csv_data object
         csvReader = csv.DictReader(file)
         csv_data = [row for row in csvReader]
     return csv_data

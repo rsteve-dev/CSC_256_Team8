756be7660d5f0909f3aef91ff97d1ca1d1ba577a
A1denMorris <aidendmorris1@gmail.com>
2023-10-24 15:23:02 -0400

    Added "Add To Cart" Button

--- Selenium Lab/WebApp/main.py
+++ Selenium Lab/WebApp/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import json
 import csv
 
@@ -6,16 +6,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def start_app():
-    csv_data = get_csv_data()
+    csv_data = get_csv_data('./static/books_short.csv')
     # Render the search.html file in templates folder and pass the csv data through
     return render_template('search.html', csv_data=csv_data)
 
+@app.route("/process", methods=['POST'])
+def process():
+    data = request.get_json()
+
+    return data
+
 # Funtion to get the CSV file's data
-def get_csv_data():
+def get_csv_data(loc):
     csv_data = []
     # Open the file and force UTF-8 encoding
-    with open('./static/books_short.csv', encoding="utf8") as file:
+    with open(loc, encoding="utf8") as file:
         # Read the file and put each row in a csv_data object
         csvReader = csv.DictReader(file)
         csv_data = [row for row in csvReader]
     return csv_data
+

2a7682e3d1322dc77bb886c35644d849ee6ec2ee
rsteve-dev <srotich@caysis.com>
2023-11-19 18:22:41 -0500

    update environment & collection

--- API Test Lab/bookstore/app/routes/main_routes.py
+++ API Test Lab/bookstore/app/routes/main_routes.py
@@ -1,26 +1,29 @@
 from ..utils import helpers
-from flask import Blueprint ,jsonify,current_app,request
+from flask import Blueprint, jsonify, current_app, request
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
 
-@main.route('/api/books', methods=['GET'])
+@main.route("/api/books", methods=["GET"])
 def get_books():
-    #title,subtitle,authors,categories,published_year,num_pages
-    attribute =  request.args.get('attribute')
-    value =request.args.get('value')
-    
+    # title,subtitle,authors,categories,published_year,num_pages
+    attribute = request.args.get("attribute")
+    value = request.args.get("value")
+    print(f"attribute: {attribute}")
+    print(f"value: {value}")
+
     try:
         books = helpers.get_books_from_csv(helpers.csv_path)
         if attribute and value:
-            books=helpers.filter_books(helpers.csv_path,attribute,value.strip("'"))
-            #return books,200
-            return jsonify(books), 200
-            
+            filtered_books = helpers.filter_books(
+                helpers.csv_path, attribute, value.strip("'")
+            )
+            # return books,200
+            return jsonify(filtered_books), 200
+
         else:
             books = helpers.get_books_from_csv(helpers.csv_path)
             return jsonify(books), 200
     except Exception as e:
         current_app.logger.error(f"Error occurred: {e}")
-        return jsonify({'error': str(e)}), 500
-        
+        return jsonify({"error": str(e)}), 500
